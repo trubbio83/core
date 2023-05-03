ebf1cd747874dba9670747ffd7910a51ddbca817
Luca Trubbiani <luca.trubbiani@gmail.com>
2023-05-03 16:17:58 +0200

    Init spring boot project

---
+++ src/test/java/it/smartcommunitylabdhub/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package it.smartcommunitylabdhub.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
